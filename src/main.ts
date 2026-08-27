468383983ec2fe3bae04a95cb7c6f1d5cf8ee432
brady <bradythomas690@gmail.com>
2026-08-27 09:57:54 -0500

    feat(config): config module setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import 'reflect-metadata';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('app.port', 3000);
+
+  await app.listen(port);
 }
 
 void bootstrap();
