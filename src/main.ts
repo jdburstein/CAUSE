265dddf3a901e45de1fda42d528cc62e88481e21
brady <bradythomas690@gmail.com>
2026-09-05 07:26:52 -0500

    feat(app): added api prefix and versioning

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import { VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -6,6 +7,11 @@ import { EnvironmentVariables } from './config/env';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
   const configService = app.get<ConfigService<EnvironmentVariables, true>>(
     ConfigService,
   );
